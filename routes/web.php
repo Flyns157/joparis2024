0280229bbd044463282f158c5d3d9c23ff583fab
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-13 14:48:54 +0200

    Question 1.2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TacheController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/taches', [TacheController::class,'index'])->name('taches.index');
